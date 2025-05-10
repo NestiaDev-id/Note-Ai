aa9d86ee0bbc33e45b739e8d2ad09da62f501633
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-10 17:47:10 +0700

    add delete note action

--- src/action/note.ts
+++ src/action/note.ts
@@ -3,7 +3,22 @@
 import { getUser } from "@/auth/server";
 import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
+import DeleteNotedButton from "../components/DeleteNoteButton";
 
+export const deleteNoteAction = async (noteId: string) => {
+  try {
+    const user = await getUser();
+    if (!user) throw new Error("You must be logged in to delete a note");
+
+    await prisma.note.delete({
+      where: { id: noteId, authorId: user.id },
+    });
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
 export const createNoteAction = async (noteId: string) => {
   try {
     const user = await getUser();
---
+++ src/components/DeleteNoteButton.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { Loader2, Trash2 } from "lucide-react";
+import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import { deleteNoteAction } from "@/action/note";
+
+type Props = {
+  noteId: string;
+  deleteNoteLocally: (noteId: string) => void;
+};
+
+function DeleteNotedButton({ noteId, deleteNoteLocally }: Props) {
+  const router = useRouter();
+  const noteIdParam = useSearchParams().get("noteId") || "";
+  const [isPending, startTransition] = useTransition();
+
+  const handleDeleteNote = () => {
+    startTransition(async () => {
+      const { errorMessage } = await deleteNoteAction(noteId);
+
+      if (!errorMessage) {
+        toast.success("Note deleted", {
+          description: "Note deleted successfully!",
+        });
+        deleteNoteLocally(noteId);
+        if (noteId === noteIdParam) {
+          router.replace("/");
+        }
+      } else {
+        toast.error(errorMessage);
+      }
+    });
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="absolute top-1/2 right-2 size-7 -translate-y-1/2 p-0 opacity-0 group-hover/item:opacity-100 [&_svg]:size-3"
+          variant="ghost"
+        >
+          <Trash2 />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure want to delete this note?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleDeleteNote}
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90 w-24"
+          >
+            {isPending ? <Loader2 className="animate-spin" /> : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteNotedButton;
